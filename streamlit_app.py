8541dcf17fd88a4430ba41d4fac4008497cbc674
patobordenave <pato.22.89@gmail.com>
2024-09-30 12:08:24 -0300

    First commit

---
+++ streamlit_app.py
@@ -0,0 +1,45 @@
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+existing_data = conn.read(worksheet="Hoja1", usecols=list(range(6)), ttl=5)
+existing_data = existing_data.dropna(how="all")
+
+# mostrar el dataframe con los datos
+# st.dataframe(existing_data)
+
+col_izq, col_der = st.columns((2,1))
+with col_izq:
+    st.markdown("<h1 style='text-align: center'>Cumple Brune y Nina</h1>", unsafe_allow_html=True)
+with col_der:
+    st.image('./assets/kitty2.gif')
+
+with st.form(key="Form1"):
+    col1, col2 = st.columns(2)
+    with col1:
+        nombre = st.text_input("Nombre")
+    with col2:
+        apellido = st.text_input("Apellido")
+    telefono = st.text_input("Telefono")
+    value = [[nombre, apellido, telefono]]
+    submit_button = st.form_submit_button("Enviar")
+if submit_button and nombre != '' and apellido != '' and telefono != '':
+    agregar_asistente = pd.DataFrame([{
+        "Nombre": nombre,
+        "Apellido": apellido,
+        "Telefono": telefono
+    }])
+    update_df = pd.concat([existing_data, agregar_asistente], ignore_index=True)
+
+    # Update Google Sheets with the new vendor data
+    conn.update(worksheet="Hoja1", data=update_df)
+
+    left_co, cent_co,last_co = st.columns((1, 2, 1))
+    with cent_co:
+        st.success('Gracias por venir al cumple!')
+        st.image('./assets/gifkitty.gif')
+elif submit_button and nombre == '' and apellido == '' and telefono == '':
+    st.error("Te olvidaste de ingresar alguno de los datos")
